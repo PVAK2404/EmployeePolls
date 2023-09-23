f2f29f1e2de6fb28da44cda3ebc0c5a9a308fd6d
PVAK2404 <khoapva.dev@gmail.com>
2023-09-24 06:14:49 +0700

    fix: fix some code

--- src/stores/Gate.jsx
+++ src/stores/Gate.jsx
@@ -1,15 +1,13 @@
-import { getPersistor } from '@rematch/persist';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/lib/integration/react';
 
 import { store } from './store';
 
-const persistor = getPersistor();
+// const persistor = getPersistor();
 
 export default function Gate({ children }) {
   return (
-    <PersistGate persistor={persistor}>
-      <Provider store={store}>{children}</Provider>
-    </PersistGate>
+    // <PersistGate persistor={persistor}>
+    <Provider store={store}>{children}</Provider>
+    // </PersistGate>
   );
 }
--- src/stores/store.js
+++ src/stores/store.js
@@ -1,13 +1,11 @@
 import { init } from '@rematch/core';
-import persistPlugin from '@rematch/persist';
-import storage from 'redux-persist/lib/storage';
 
 import { models } from './models';
 
-const persistConfig = {
-  key: 'root',
-  storage,
-  whitelist: ['authStore'],
-};
+// const persistConfig = {
+//   key: 'root',
+//   storage,
+//   whitelist: ['authStore'],
+// };
 
-export const store = init({ models, plugins: [persistPlugin(persistConfig)] });
+export const store = init({ models });
